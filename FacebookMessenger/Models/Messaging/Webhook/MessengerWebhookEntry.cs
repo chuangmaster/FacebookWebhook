9518ef6cbe6408a87f5f93fca54f53e6b03e03ba
chuangmaster <masterchuang@urtody.com>
2019-11-15 18:10:42 +0800

    remove superfluous namespace

--- FacebookMessenger/Models/Messaging/Webhook/MessengerWebhookEntry.cs
+++ FacebookMessenger/Models/Messaging/Webhook/MessengerWebhookEntry.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FacebookWebhook.Models;
-using FacebookWebhook.Models;
 using Newtonsoft.Json;
 
-namespace FacebookMessenge.Models.Messaging.Webhook
+namespace FacebookMessenger.Models.Messaging.Webhook
 {
     public class MessengerWebhookEntry : WebhookBaseEntry
     {
