a20e5fb22ea4ee436efaf2c3709e229693297f61
Musk <chuang.master@gmail.com>
2019-12-05 01:40:44 +0800

    Add PassingThreadControlContainer

---
+++ FacebookMessenger/Models/Messaging/PassingThreadControlContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FacebookMessenger.Models.Messaging
+{
+    public class PassingThreadControlContainer
+    {
+        [JsonProperty("recipient")]
+        public string Recipient { get; set; }
+
+        [JsonProperty("target_app_ids")]
+        public string SecondaryID { get; set; }
+
+        [JsonProperty("metadata")]
+        public string MetaData { get; set; }
+    }
+}
